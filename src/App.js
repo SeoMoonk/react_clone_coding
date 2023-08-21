3a1280db48293e3f036ae1b41ef934051785c635
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-08-21 14:31:11 +0900

    Food Props 3Versions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
 import React from 'react';
 
-function Potato () {
-    return(
-        <div>
-            <h1>I Like Potato</h1>
-        </div>
-    );
+//V1
+function Food({fav}) {
+  return (
+    <div>
+      <h1>I Like {fav}</h1>
+    </div>
+  );
 }
+
+//V2 (Props)
+function Food2(props) { //props => properties
+  return (
+    <div>
+      <h1>I Like {props.fav}</h1>
+    </div>
+  );
+}
+
+//V3
+function Food3({name}){
+  return (
+    <div>
+      <h1>I Like {name}</h1>
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: 'Coffee',
+    image: ''
+  },
+  {
+    name: 'Cutlet',
+    image: ''
+  },
+  {
+    name: 'RiceCake',
+    image: ''
+  }
+]
 
 function App() {
   return (
     <div>
       <h1>Hello123</h1>
-      <hr/>
-      <Potato />
-      <Potato />
-      <Potato />
+      <hr />
+      <Food fav="Potato" />
+      <Food fav="Orange" />
+      <Food fav="Ice Cream" />
+      <hr />
+      <Food2 fav="Chiken" />
+      <Food2 fav="Milk" />
+      <Food2 fav="Apple" />
+      <hr />
+      {foodILike.map((dish) => {
+        return <Food3 name={dish.name} />
+      })}
     </div>
   );
 }
