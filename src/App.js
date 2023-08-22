2caffc0c2f4cfdfaefb3da32e876138b206dc553
SeoMoonk <39723465+SeoMoonk@users.noreply.github.com>
2023-08-22 15:53:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './routes/About';
-import Home from './routes/Home';
-import Navigation from './components/Navigation';
-import Detail from './routes/Detail';
-
-function App() {
-
-  
-  /* React Router v5 에서는 HashRouter -> Route 가 가능 했지만
-  v6 부터는 Router -> Routes -> Route 구조를 사용한다. */
-
-  return (
-    <Router>
-      <Navigation />  {/* 버전 업 되면서, Route로 감싸주지 않으면 사용할 수 없음. => Routes 밖으로 빼줌*/}
-      <Routes>
-        {/* 버전 업 되면서, component props가 아닌 element props를 받음.*/}
-        <Route path="/" exact={true} element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/movie-detail" element={<Detail />} />
-      </Routes>
-    </Router>
-  );
+import React from "react";
+import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from './routers/About';
+import Home from "./routers/Home";
+import Navigation from "./components/Navigation";
+import Detail from "./routers/Detail";
+function App(){
+    return (
+        <BrowserRouter>
+            <Navigation /> {/* 버전 업 되면서, Route로 감싸주지 않으면 사용할 수 없음. => Routes 밖으로 빼줌*/}
+            <Routes>
+                {/* 버전 업 되면서, component props가 아닌 element props를 받음.*/}
+                <Route path="/about" element={<About/>}></Route>
+                <Route path="/:page" element={<Home/>} />
+                <Route path="/detail-movie" element={<Detail />}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
