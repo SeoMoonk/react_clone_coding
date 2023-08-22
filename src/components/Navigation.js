62df3f1580d5c7c63bb7700d50b7f212d0226554
SeoMoonk <39723465+SeoMoonk@users.noreply.github.com>
2023-08-22 15:55:40 +0900

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ import './Navigation.css';
 function Navigation() {
     return (
         <div className="nav">
-            <Link to="/">Home</Link>
+            <Link to="/1">Home</Link>
             <Link to="/about">About</Link>
         </div>
     );
